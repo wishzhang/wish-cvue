2d6682ce58e17e63b0be34dee1c5e61fb64f82ca
Wang Zhang <1535703141@qq.com>
2022-05-31 08:14:24 +0800

    edit config

--- play/vite.config.ts
+++ play/vite.config.ts
@@ -2,14 +2,13 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { cvueRoot, pkgRoot } from '../build/utils/paths'
 import { resolve } from 'path'
-import path from 'path'
 
 export default defineConfig({
   resolve: {
     alias: [
       {
         find: /^cvue$/,
-        replacement: path.resolve(cvueRoot, 'index.ts'),
+        replacement: resolve(cvueRoot, 'index.ts'),
       },
       {
         find: /^cvue\/(.*)$/,

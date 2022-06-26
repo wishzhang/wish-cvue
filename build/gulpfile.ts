815aa680bb629fffd9cd093bfcff213c325f2772
Wang Zhang <1535703141@qq.com>
2022-06-26 12:31:12 +0800

    edit config

--- build/gulpfile.ts
+++ build/gulpfile.ts
@@ -1,15 +1,30 @@
-import { mkdir } from 'fs/promises'
+import path from 'path'
+import { mkdir, copyFile } from 'fs/promises'
 import { series, parallel } from 'gulp'
 import { run } from './utils/process'
 import { runTask, withTaskName } from './utils/gulp'
 import { cvueOutput } from './utils/paths'
+
+export const copyFullStyle = async () => {
+  await mkdir(path.resolve(cvueOutput, 'dist'), { recursive: true })
+  await copyFile(
+    path.resolve(cvueOutput, 'theme-chalk/index.css'),
+    path.resolve(cvueOutput, 'dist/index.css')
+  )
+}
 
 export default series(
   withTaskName('clean', () => run('pnpm run clean')),
   withTaskName('createOutput', () => mkdir(cvueOutput, { recursive: true })),
 
   parallel(
-    runTask('buildFullBundle')
+    runTask('buildFullBundle'),
+    series(
+      withTaskName('buildThemeChalk', () =>
+        run('pnpm run -C packages/theme-chalk build')
+      ),
+      copyFullStyle
+    )
   )
 )
 

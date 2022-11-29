7c9df5a4f5324abe2efa1c2493bed600da8de768
wishzhang <wishzhang.io@qq.com>
2022-11-29 21:18:28 +0800

    chore: doc contrusture

--- docs/docs/.vuepress/config.ts
+++ docs/docs/.vuepress/config.ts
@@ -1,42 +1,50 @@
-// module.exports = {
-//   title: 'Hello VuePress',
-//   description: 'Just playing around',
-//   themeConfig: {
-//     sidebarDepth: 2,
-//     sidebar: [
-//       {
-//         title: 'Basic 基础组件',   // 必要的
-//         collapsable: false, // 可选的, 默认值是 true,
-//         children: [
-//           ['/basic/button', 'Button 按钮']
-//         ]
-//       }
-//     ]
-//   }
-// }
-
 import { defineUserConfig, defaultTheme } from 'vuepress'
+import { docsearchPlugin } from '@vuepress/plugin-docsearch'
 
 export default defineUserConfig({
   lang: 'zh-CN',
-  title: '你好， VuePress ！',
+  title: 'CVue',
   description: '这是我的第一个 VuePress 站点',
+  plugins: [docsearchPlugin({} as any)],
   theme: defaultTheme({
     // 侧边栏数组
     // 所有页面会使用相同的侧边栏
     sidebar: [
-      // SidebarItem
       {
-        text: 'Basic 基础组件',
+        text: '指南',
         children: [
-          // SidebarItem
           {
-            text: 'Button 按钮',
-            link: '/basic/button',
-            children: [],
+            text: '安装',
+            link: '/guide/setup',
+          },
+          {
+            text: '快速启动',
+            link: '/guide/boot',
           },
         ],
       },
+      {
+        text: '组件',
+        children: [
+          { text: 'Card 卡片', link: '/comp/card' },
+          { text: 'Checkbox 多选框', link: '/comp/checkbox' },
+          { text: 'Date Picker 日期选择器', link: '/comp/date-picker' },
+          { text: 'Dialog 对话框', link: '/comp/dialog' },
+          { text: 'Form 表单', link: '/comp/form' },
+          { text: 'Input 输入框', link: '/comp/input' },
+          {
+            text: 'Input Number Range 数字范围',
+            link: '/comp/input-number-range',
+          },
+          { text: 'Radio 单选框', link: '/comp/radio' },
+          { text: 'Select 选择器', link: '/comp/select' },
+          { text: 'Table 表格', link: '/comp/table' },
+          { text: 'Time Picker', link: '/comp/time-picker' },
+          { text: 'Upload', link: '/comp/upload' },
+        ],
+      },
     ],
+    // repository
+    repo: 'https://github.com/wishzhang/wish-cvue',
   }),
 })
